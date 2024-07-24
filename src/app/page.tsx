afdcb6a31d342864099c2073df2d8754e3aa053f
why <107970248+whyang949@users.noreply.github.com>
2024-07-24 19:11:57 +0800

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+     home
+    </div>
+  );
+}
