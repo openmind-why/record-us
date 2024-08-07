60ed0f27618dc79078e6cb96ac328d19628a69ad
wanghongyang <549492949@qq.com>
2024-08-07 17:29:45 +0800

    ✨ feat: test

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3AHHY6POJiMsiYshVunTYIFQPrSuyMAmji0wx2uuCMU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wkln0qs4IPg/ahmoFf+xQXQWqBNoc5G8P8Bq9L7AzEI=\"\n}"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { sql } from "@vercel/postgres";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  // const { rows } = await sql `SELECT * from CARTS where user_id=111`;
+  // console.log(rows,'=============');
+  
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
      home
